c90c63ea242c1906ed0e87b141d2316d23ddfb2e
NavpreetST <999navtoor@gmail.com>
2025-07-06 22:44:07 +0200

    exercise 2

---
+++ src/exercise2.js
@@ -0,0 +1,26 @@
+import styles from "./App.module.css";
+import { User } from "./list_user";
+
+export default function List_ternary_exercise() {
+    //loop through planet and display their name. Only display if they are a gas planet.
+    const planet = [
+        { name: "Mars", isGasPlanet: false },
+        { name: "Earth", isGasPlanet: false },
+        { name: "Jupiter", isGasPlanet: true },
+        { name: "Venus", isGasPlanet: false },
+        { name: "Uranus", isGasPlanet: true },
+        { name: "Neptune", isGasPlanet: true },
+        { name: "Cybertron", isGasPlanet: false },
+
+    ];
+
+
+
+
+
+
+
+
+
+
+}
